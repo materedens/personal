c00a9f0b5db0da0d83be19e870c677bb69350e0f
Denis Matere <denismatere@gmail.com>
2022-07-30 16:12:03 +0300

    Email Sending Using Emailjs

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,22 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
-const Contact = () => {
+export const Contact = () => {
+ const form = useRef();
+
+const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_xxbjzan', 'template_hqdys1b', form.current, 'C-tjjnqV8g8SNPU3g')
+      .then((result) => {
+          console.log(result.text);
+          console.log("message sent");
+      }, (error) => {
+          console.log(error.text);
+      });
+      e.target.reset();
+  };
+    
   return (
     <section id='contact' className='contact section-bg'>
         <div className='container'>
@@ -10,13 +26,13 @@ const Contact = () => {
             </div>
             <div className='row justify-content-center'>
                 <div className='col-xl-12 col-lg-12 mt-4'>
-                    <form className='form'>
+                    <form className='form' ref={form} onSubmit={sendEmail}>
                         <div className='row'>
                             <div className='col-md-6 form-group'>
-                                <input type='text' name='name' className='form-control' id='name' placeholder='Name' required></input>
+                                <input type='text' name='user_name' className='form-control' placeholder='Name' required></input>
                             </div>
                             <div className="col-md-6 form-group mt-3 mt-md-0">
-								<input type="email" className="form-control" name="email" id="email" placeholder="Your Email" required />
+								<input type="email" className="form-control" name="user_email" placeholder="Your Email" required />
 							</div>
                         </div>
                         <div className="form-group mt-3">
@@ -25,7 +41,9 @@ const Contact = () => {
 						<div className="form-group mt-3">
 							<textarea className="form-control" name="message" rows="5" placeholder="Message" required></textarea>
 						</div><br></br>
-                        <div className="text-center"><button type="submit">Send Message</button></div>
+                        <div className="text-center">
+                            <button type="submit" value='Send'>Send Message</button>
+                        </div>
                     </form>
                 </div>
             </div>
