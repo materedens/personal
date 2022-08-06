f69e749d0c8dba9b2951fd76a865a7c672bcb29e
Denis Matere <denismatere@gmail.com>
2022-08-06 18:12:51 +0300

    Preloader

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from "react";
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 // import Particles from 'react-particles-js';
@@ -8,12 +9,45 @@ import About from './components/About';
 import Contact from './components/Contact';
 import Services from './components/Services';
 import ScrollToTop from 'react-scroll-to-top';
-// import Preloader from './components/Preloader';
 
 
-function App() {
+function App() { 
+  const [data, setData] = useState([]);
+  const [loading, setloading] = useState(undefined);
+  const [completed, setcompleted] = useState(undefined);
+
+  useEffect(() => {
+    setTimeout(() => {
+      fetch("https://jsonplaceholder.typicode.com/posts")
+        .then((response) => response.json())
+        .then((json) => {
+          console.log(json);
+          setData(json);
+          setloading(true);
+
+          setTimeout(() => {
+            setcompleted(true);
+          }, 1000);
+        });
+    }, 2000);
+  }, []);
+
   return (
     <>
+    {!completed ? (
+      <>
+      {!loading ? (
+        <div className='spinner'>
+          <span>DENNIS MATERE......</span>
+          <div className='half-spinner'></div>
+        </div>
+      ) : (
+        <div className='completed'>&#x2713;</div>
+      )}
+      </>
+    ) : (
+      <>
+   
       {/* <Particles 
        className="particles-canvas"
          params={{
@@ -43,8 +77,9 @@ function App() {
       <Contact />
       <Footer />
       <ScrollToTop style={{ marginTop: "150vh" }} smooth color='#ef4035' />
-
-    </>
+      </>
+     )}
+     </>
   );
 }
 
